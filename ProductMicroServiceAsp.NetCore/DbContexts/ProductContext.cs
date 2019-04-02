97206d990e5180d59f040311588e0a271f663337
soakes84 <spencer.oakes84@gmail.com>
2019-04-02 19:40:20 -0400

    setting up ProductContext

---
+++ ProductMicroServiceAsp.NetCore/DbContexts/ProductContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroServiceAsp.NetCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroServiceAsp.NetCore.DbContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Books",
+                    Description = "Reading Material",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Outdoors",
+                    Description = "Hiking equipment and accessories",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Chickens",
+                    Description = "Everything you need for raising chickens",
+                },
+                 new Category
+                 {
+                     Id = 4,
+                     Name = "Sports",
+                     Description = "All Sports",
+                 }
+            );
+        }
+    }
+}
