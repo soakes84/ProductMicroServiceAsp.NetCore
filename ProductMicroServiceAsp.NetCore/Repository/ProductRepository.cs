7f7ab0cdabeb716ffb9af347585a9dc2315f0608
soakes84 <spencer.oakes84@gmail.com>
2019-04-02 21:52:30 -0400

    ProductRepository

---
+++ ProductMicroServiceAsp.NetCore/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductMicroServiceAsp.NetCore.DbContexts;
+using ProductMicroServiceAsp.NetCore.Model;
+
+namespace ProductMicroServiceAsp.NetCore.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
--- ProductMicroServiceAsp.NetCore/Startup.cs
+++ ProductMicroServiceAsp.NetCore/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ProductMicroServiceAsp.NetCore.DbContexts;
+using ProductMicroServiceAsp.NetCore.Repository;
 
 namespace ProductMicroServiceAsp.NetCore
 {
@@ -27,6 +30,7 @@ namespace ProductMicroServiceAsp.NetCore
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
+            services.AddTransient<IProductRepository, ProductRepository>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
